e77d537e719262734724453970aeae0caf19ce1d
Divyam Vashishth <divyam@infocusp.in>
2022-04-21 07:31:32 +0000

    Made changes to model class

--- model_class.py
+++ model_class.py
@@ -1,6 +1,7 @@
-from imports.py import *
-from encoding_class.py import *
-from attention_class.py import *
+from imports.py import tf
+from imports.py import Dense
+from encoding_class.py import Encodings
+from attention_class.py import Attention
 
 
 
@@ -21,11 +22,15 @@ class _Model(tf.keras.layers.Layer):
     att_op, att_scores = self.attention(op)
 
     if(self.agg_method == 'TOKEN'):
-      op = att_op[:,0,:]
+      #op = att_op[:,0,:]
+      op = tf.squeeze(tf.gather(att_op, [0], axis=1))
     else:
-      op = tf.math.reduce_sum(att_op[:, 1:, :], axis = 1)
+      #op = tf.math.reduce_sum(att_op[:, 1:, :], axis = 1)
+      op = tf.reduce_sum(tf.gather(att_op, list(range(1, att_op.shape[1])), axis=1), axis=1)
 
     op = self.head(op)
 
 
     return op, att_scores
+
+
