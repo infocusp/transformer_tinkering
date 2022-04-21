63a3aa313298d24027383847d8d360b78a26b6c9
Divyam Vashishth <divyam@infocusp.in>
2022-04-21 07:32:34 +0000

    Changes to modelling file

--- modelling.py
+++ modelling.py
@@ -1,11 +1,13 @@
-from imports.py import *
-from data_generator.py import *
-from encoding_class.py import *
-from attention_class.py import *
-from model_class.py import *
+from imports.py import Embedding, Dense, Input
+from imports.py import Model, Adam, K
+from imports.py import plt, ReduceLROnPlateau, np
+from config.py import Config
+from data_generator.py import Generate_data
+from model_class.py import _Model
 
 
 input_shape = (512)
+config = Config()
 input = Input(input_shape)
 op, att_scores = _Model(config)(input)
 output = Dense(1, activation='linear')(op)
