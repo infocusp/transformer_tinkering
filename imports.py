2aea99c53e7ea26dc57086277412d3c581d03e42
Divyam Vashishth <divyam@infocusp.in>
2022-04-21 07:30:43 +0000

    Replace imports.py

--- imports.py
+++ imports.py
@@ -15,15 +15,3 @@ from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 import numpy as np
-
-
-config = {
-    'num_heads' : 1,
-    'num_layers': 1,
-    'emb_dim': 128,
-    'seq_length': 512,
-    'vocab_size': 2,
-    'head_size': 64, #size of single dense layer head
-    'pos_embedding': True, #True or False weather to learn positional embeddings
-    'agg_method': 'TOKEN' #one of TOKEN or SUM
-}
