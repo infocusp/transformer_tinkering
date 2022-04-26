80077543a516fe0b75c5081c18ee52f4e3f0531e
Divyam Vashishth <divyam@infocusp.in>
2022-04-26 10:25:36 +0000

    With latest changes

--- model.py
+++ model.py
@@ -1,39 +1,50 @@
-from hyperparms.py import tf
-from hyperparms.py import Dense
-from hyperparms.py import Embedding
-from hyperparms.py import np
-
+from tensorflow.keras.layers import Embedding, Dense, Input
+import tensorflow as tf
+import numpy as np
 
 
 class Encodings(tf.keras.layers.Layer):
 
   '''
-    This class takes input tokens and return the embeddings with class token appended and positional embeddings added.
+    This class takes return the embeddings with positional embeddings added.
 
     Alternating sin cos  positional embeddings are used, the position matrix P is filled using the following rule
     P(k, 2i) = sin(k/n^(2i/d))
     P(k, 2i + 1) = cos(k/n^(2i/d))
 
+    Attributes:
+    	emb_dim: the required dimension for our resultant embeddings
+    	seq_length: the length of the input sequence
+    	vocab_size: the size of our vocab
+    	pos_embedding: Boolean flag used to weather include positional encodings or not
+    	embedding_type: RANDOM/SIN_COS which type of positional embedding to use , random or alternate sin cos
+
   '''
 
-  def __init__(self, config):
+  def __init__(self, emb_dim, seq_length, vocab_size, pos_embedding, embedding_type):
+
+    '''Initializes the class with variables and Embedding layers '''
 
     super().__init__()
-    self.embedding_dim = config['emb_dim']
-    self.seq_length = config['seq_length']
-    self.vocab_size = config['vocab_size']
-    self.pos_embedding_flag = config['pos_embedding']
+    self.embedding_dim = emb_dim
+    self.seq_length = seq_length
+    self.vocab_size = vocab_size
+    self.pos_embedding_flag = pos_embedding
+    self.embedding_type = embedding_type
     self.batch_size = 64
     self.embedding = Embedding(self.vocab_size + 2, self.embedding_dim, input_length = self.seq_length)
-    #self.pos_embedding = Embedding(self.seq_length + 1, self.embedding_dim)
-    self.pos_embedding_mat = self.get_pos_emb_mat(self.seq_length, self.embedding_dim)
-    self.pos_embedding = Embedding(self.seq_length + 1, self.embedding_dim, weights = [self.pos_embedding_mat], trainable= False)
-    
+    self.random_pos_embedding = Embedding(self.seq_length + 1, self.embedding_dim)
+    self.sin_pos_embedding_mat = self.get_sin_pos_emb_mat(self.seq_length, self.embedding_dim)
+    self.sinusodial_pos_embedding = Embedding(self.seq_length + 1, self.embedding_dim, weights = [self.sin_pos_embedding_mat], trainable= False)
 
-  def get_pos_emb_mat(self, seq_len, d, n = 10000):
+
+
+
+
+  def get_sin_pos_emb_mat(self, seq_len, d, n = 10000):
 
     '''
-      This function gives the positional embedding matrix 
+      This function gives the positional embedding matrix
     '''
 
     P = np.zeros((seq_len + 1, d))
@@ -51,10 +62,16 @@ class Encodings(tf.keras.layers.Layer):
 
 
   def call(self, batch):
+
+    '''Performs transformations on our given batch of data'''
+
     embedding_out = self.embedding(batch)
 
-    if(self.pos_embedding_flag):
-      pos_embedding = self.pos_embedding(tf.range(start = 0, limit = self.seq_length + 1, delta=1))
+    if(self.pos_embedding_flag and self.embedding_type == 'SIN_COS'):
+      pos_embedding = self.sinusodial_pos_embedding(tf.range(self.seq_length + 1))
+      embedding_out = embedding_out + pos_embedding
+    else:
+      pos_embedding = self.random_pos_embedding(tf.range(self.seq_length + 1))
       embedding_out = embedding_out + pos_embedding
 
 
@@ -65,28 +82,47 @@ class Encodings(tf.keras.layers.Layer):
 class Attention(tf.keras.layers.Layer):
 
   '''
-    This class implements the Attention layer mechanism and returns the attention output and attention scores for each attention head
+    This class implements the Multi head Attention layer mechanism and returns the attention output and attention scores for each attention head
+
+    The query, key, and value matrices are first calculated and then the attention scores are calculated with each input token as a query. The following operations are performed
+
+    	softmax((Q*K_T)/sqrt(attention_head_size))*V
+
+    	Q: Query matrix
+    	K_T: Key matrix transpose
+    	V: Value matrix
+
+    Attributes:
+    	num_heads: Number of heads we want in out attention layer
+    	emb_dim: the required dimension for our resultant embeddings
   '''
 
-  def __init__(self,config):
+  def __init__(self, num_heads, emb_dim):
+
+    '''Initiates all the variables and layers required to perform attention'''
+
     super().__init__()
 
-    self.num_att_heads = config['num_heads']
-    self.attention_head_size = int(config['emb_dim'] / self.num_att_heads)
+    self.num_att_heads = num_heads
+    self.attention_head_size = int(emb_dim / self.num_att_heads)
     self.all_head_size = self.num_att_heads * self.attention_head_size
 
     self.query = Dense(self.all_head_size)
     self.key = Dense(self.all_head_size)
     self.value = Dense(self.all_head_size)
-    self.out = Dense(config['emb_dim'])
+    self.out = Dense(emb_dim)
 
   def split_heads(self, input_layer, hidden_states_shape):
 
-    return tf.reshape(input_layer, (hidden_states_shape[0], self.num_att_heads, 
+    '''Splits the input matrix between attention heads'''
+
+    return tf.reshape(input_layer, (hidden_states_shape[0], self.num_att_heads,
                                  hidden_states_shape[1],
                                  self.attention_head_size))
 
   def call(self, hidden_states):
+
+    '''Performs transformations on input batch to get our attention scores and output'''
 
     #getting the query , key and value vectors
     mixed_query_layer = self.query(hidden_states)
@@ -97,23 +133,23 @@ class Attention(tf.keras.layers.Layer):
 
 
     #Dividing query keay and value vectors between given number of attention heads
-    # query_layer = tf.reshape(mixed_query_layer, shape = (hidden_states_shape[0], self.num_att_heads, 
+    # query_layer = tf.reshape(mixed_query_layer, shape = (hidden_states_shape[0], self.num_att_heads,
     #                              hidden_states_shape[1],
     #                              self.attention_head_size))
-    
 
-    # key_layer = tf.reshape(mixed_key_layer, shape = (hidden_states_shape[0], self.num_att_heads, 
+
+    # key_layer = tf.reshape(mixed_key_layer, shape = (hidden_states_shape[0], self.num_att_heads,
     #                              hidden_states_shape[1],
     #                              self.attention_head_size))
-    
-    # value_layer = tf.reshape(mixed_value_layer, shape = (hidden_states_shape[0], self.num_att_heads, 
+
+    # value_layer = tf.reshape(mixed_value_layer, shape = (hidden_states_shape[0], self.num_att_heads,
     #                              hidden_states_shape[1],
     #                              self.attention_head_size))
 
     query_layer = self.split_heads(mixed_query_layer, hidden_states_shape)
 
     key_layer = self.split_heads(mixed_key_layer, hidden_states_shape)
-    
+
     value_layer = self.split_heads(mixed_value_layer, hidden_states_shape)
 
 
@@ -130,7 +166,7 @@ class Attention(tf.keras.layers.Layer):
     context_layer = tf.reshape(context_layer, shape=( hidden_states_shape[0],
                                                          hidden_states_shape[1],
                                                          hidden_states_shape[2]))
-    
+
     att_output = self.out(context_layer)
 
     return att_output, attention_probs
@@ -140,17 +176,43 @@ class Attention(tf.keras.layers.Layer):
 
 class _Model(tf.keras.layers.Layer):
 
-  def __init__(self, config):
+  ''' This is a wrapper class for attention and encoding class
+
+  	Our output from the attention layer is passed through perceptron layer for transformation,
+  	output of which is returned.
+
+     Attributes:
+
+     	emb_dim: the required dimension for our resultant embeddings
+    	seq_length: the length of the input sequence
+    	vocab_size: the size of our vocab
+    	pos_embedding: Boolean flag used to weather include positional encodings or not
+    	num_heads: Number of heads we want in out attention layer
+    	head_size: the number of units we want in our final dense layer
+    	agg_method: TOKEN/SUM tells us how to aggregate our attention output to feed to final dense layer.
+    	            TOKEN: the CLS token vectors are fetched and fed into the dense layer
+    	            SUM: The attention output is added and frd into the dense layer
+
+
+  '''
+
+  def __init__(self, emb_dim, seq_length, vocab_size, pos_embedding, num_heads, head_size, agg_method, embedding_type ):
+
+    '''This initializes the variables andlayers required'''
+
     super().__init__()
 
-    self.encodings = Encodings(config)
-    self.attention = Attention(config)
-    self.agg_method = config['agg_method']
-    self.head_dim = config['head_size']
+    self.encodings = Encodings(emb_dim, seq_length, vocab_size, pos_embedding, embedding_type)
+    self.attention = Attention(num_heads, emb_dim)
+    self.agg_method = agg_method
+    self.head_dim = head_size
     self.head = Dense(self.head_dim)
 
 
   def call(self, input):
+
+    '''Performs all the transformations on input batch and returns the attention scores and output'''
+
     op = self.encodings(input)
     att_op, att_scores = self.attention(op)
 
@@ -165,7 +227,3 @@ class _Model(tf.keras.layers.Layer):
 
 
     return op, att_scores
-
-
-
-
