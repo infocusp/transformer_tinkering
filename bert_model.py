6e8a703d4e3e07a4b7b6a3cb16a5ce9d6132e6a5
Sarveshmani Tiwari <sarveshmani@infocusp.com>
2023-06-15 07:08:35 +0000

    Basic BERT Model with basic input, output and transformer encoder layer

---
+++ bert_model.py
@@ -0,0 +1,195 @@
+import torch
+import torch.nn as nn
+import math
+
+class BERTInputEncoder(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, max_seq_length):
+        super(BERTInputEncoder, self).__init__()
+        
+        self.token_embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.position_embedding = nn.Embedding(max_seq_length, embedding_dim)
+        self.segment_embedding = nn.Embedding(2, embedding_dim)  
+        
+        self.embedding_dim = embedding_dim
+        self.max_seq_length = max_seq_length
+    
+    def forward(self, input_ids, segment_ids):
+        # Token embedding
+        token_embeds = self.token_embedding(input_ids)
+        # Positional encoding
+        position_ids = torch.arange(input_ids.size(1)).unsqueeze(0).to(input_ids.device)
+        position_embeds = self.position_embedding(position_ids)
+        # Segment embedding
+        segment_embeds = self.segment_embedding(segment_ids)
+        # Summing token, positional, and segment embeddings
+        embeddings = token_embeds + position_embeds + segment_embeds
+        
+        return embeddings
+
+
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(self, hidden_size, num_heads):
+        super(MultiHeadSelfAttention, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.head_size = hidden_size // num_heads
+        
+        self.query_projection = nn.Linear(hidden_size, hidden_size)
+        self.key_projection = nn.Linear(hidden_size, hidden_size)
+        self.value_projection = nn.Linear(hidden_size, hidden_size)
+        
+        self.output_projection = nn.Linear(hidden_size, hidden_size)
+        self.layer_norm = nn.LayerNorm(hidden_size)
+    
+    def forward(self, inputs, mask=None):
+        batch_size, seq_length, hidden_size = inputs.size()
+        
+        # Projecting inputs for query, key, and value
+        queries = self.query_projection(inputs).view(batch_size, seq_length, self.num_heads, self.head_size).transpose(1, 2)
+        keys = self.key_projection(inputs).view(batch_size, seq_length, self.num_heads, self.head_size).transpose(1, 2)
+        values = self.value_projection(inputs).view(batch_size, seq_length, self.num_heads, self.head_size).transpose(1, 2)
+        
+        # Compute attention scores
+        attention_scores = torch.matmul(queries, keys.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.head_size).float())
+        
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask.unsqueeze(1).unsqueeze(2), float('-inf'))
+        
+        attention_probs = torch.softmax(attention_scores, dim=-1)
+        
+        # Apply attention scores to values
+        context = torch.matmul(attention_probs, values)
+        context = context.transpose(1, 2).contiguous().view(batch_size, seq_length, hidden_size)
+        
+        # Project back to the original hidden size and apply layer normalization
+        outputs = self.output_projection(context)
+        outputs = self.layer_norm(outputs)
+        
+        return outputs
+
+class FeedForwardNetwork(nn.Module):
+    def __init__(self, hidden_size, intermediate_size):
+        super(FeedForwardNetwork, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        
+        self.intermediate = nn.Linear(hidden_size, intermediate_size)
+        self.output = nn.Linear(intermediate_size, hidden_size)
+        self.layer_norm = nn.LayerNorm(hidden_size)
+    
+    def forward(self, inputs):
+        hidden = self.intermediate(inputs)
+        hidden = torch.relu(hidden)
+        outputs = self.output(hidden)
+        outputs = self.layer_norm(outputs)
+        
+        return outputs
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, hidden_size, num_heads, intermediate_size):
+        super(TransformerEncoderLayer, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.intermediate_size = intermediate_size
+        
+        self.attention = MultiHeadSelfAttention(hidden_size, num_heads)
+        self.feed_forward = FeedForwardNetwork(hidden_size, intermediate_size)
+        
+        self.attention_layer_norm = nn.LayerNorm(hidden_size)
+        self.feed_forward_layer_norm = nn.LayerNorm(hidden_size)
+    
+    def forward(self, inputs, mask=None):
+        attention_outputs = self.attention_layer_norm(inputs + self.attention(inputs, mask))
+        feed_forward_outputs = self.feed_forward_layer_norm(attention_outputs + self.feed_forward(attention_outputs))
+        
+        return feed_forward_outputs
+
+class MaskedLanguageModel(nn.Module):
+    def __init__(self, hidden_size, vocab_size):
+        super(MaskedLanguageModel, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.activation = nn.GELU()
+        self.layer_norm = nn.LayerNorm(hidden_size)
+        self.decoder = nn.Linear(hidden_size, vocab_size)
+    
+    def forward(self, inputs):
+        hidden_states = self.dense(inputs)
+        hidden_states = self.activation(hidden_states)
+        hidden_states = self.layer_norm(hidden_states)
+        logits = self.decoder(hidden_states)
+        
+        return logits
+
+class NextSentencePrediction(nn.Module):
+    def __init__(self, hidden_size):
+        super(NextSentencePrediction, self).__init__()
+        
+        self.hidden_size = hidden_size
+        
+        self.classifier = nn.Linear(hidden_size, 2)
+        self.activation = nn.LogSoftmax(dim=-1)
+    
+    def forward(self, inputs):
+        logits = self.classifier(inputs[:, 0, :])  
+        logits = self.activation(logits)
+        return logits
+
+
+class BERT(nn.Module):
+    def __init__(self, vocab_size, max_seq_length, hidden_size, num_heads, intermediate_size, num_layers):
+        super(BERT, self).__init__()
+
+        self.vocab_size = vocab_size
+        self.max_seq_length = max_seq_length
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.intermediate_size = intermediate_size
+        self.num_layers = num_layers
+
+        self.embedding = BERTInputEncoder(vocab_size, hidden_size, max_seq_length)
+
+        self.encoder = nn.ModuleList([
+            TransformerEncoderLayer(hidden_size, num_heads, intermediate_size) for _ in range(num_layers)
+        ])
+
+        self.mlm = MaskedLanguageModel(hidden_size, vocab_size)
+        self.nsp = NextSentencePrediction(hidden_size)
+    
+    def forward(self, input_ids, segment_ids, attention_mask):
+        # Input Embedding
+        embeddings = self.embedding(input_ids, segment_ids)
+        # Pass through each Encoder Layer
+        for layer in self.encoder:
+            embeddings = layer(embeddings, attention_mask)
+        # MLM & NSP
+        mlm_output = self.mlm(embeddings)
+        nsp_output = self.nsp(embeddings)
+
+        return mlm_output, nsp_output
+
+
+# Parameters 
+vocab_size = 30522
+max_seq_length = 128
+hidden_size = 768
+num_heads = 12
+intermediate_size = 3072
+num_layers = 12
+
+model = BERT(vocab_size, max_seq_length, hidden_size, num_heads, intermediate_size, num_layers)
+
+# Input to the Model
+input_ids = torch.randint(0, vocab_size, (1, max_seq_length))
+segment_ids = torch.zeros((1, max_seq_length), dtype=torch.long)
+attention_mask = torch.ones((1, max_seq_length), dtype=torch.long)
+
+mlm_output, nsp_output = model(input_ids, segment_ids, attention_mask)
+
+print(mlm_output.shape)  
+print(nsp_output.shape)
